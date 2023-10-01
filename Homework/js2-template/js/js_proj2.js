0ff6657ceb2bc791d63596f3afca97d72b40c9f7
EpicCoco <forthecodeman@gmail.com>
2023-10-01 19:19:15 -0400

    Finished project part 3

--- Homework/js2-template/js/js_proj2.js
+++ Homework/js2-template/js/js_proj2.js
@@ -33,3 +33,22 @@ blueButton.addEventListener("click", () => {
     let colorDiv = document.querySelector('#div_color');
     colorDiv.setAttribute("class", "bg_blue")
 });
+
+//PART 3
+let userInput = document.querySelector('#usrInput');
+let addButton = document.querySelector('#addBtn');
+let unorderedList = document.querySelector('#myUL');
+
+addButton.addEventListener("click", () => {
+    let input = userInput.value;
+    userInput.value = '';
+    let listElement = document.createElement("li");
+    listElement.innerHTML = input;
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = 'Delete';
+    listElement.appendChild(deleteButton);
+    unorderedList.appendChild(listElement);
+    deleteButton.addEventListener("click", () => {
+        unorderedList.removeChild(listElement);
+    });
+});
