0c114a1d49e6880125e05add522affe50cba56c7
EpicCoco <forthecodeman@gmail.com>
2023-10-01 19:07:04 -0400

    Finished project part 2

--- Homework/js2-template/js/dom_js.js
+++ Homework/js2-template/js/dom_js.js
@@ -11,3 +11,25 @@ pmButton.addEventListener("click", () => {
     greetingHeading.innerHTML = "Good night!";
 });
 
+//PART 2
+let redButton = document.querySelector('#red');
+let yellowButton = document.querySelector('#yellow');
+let greenButton = document.querySelector('#green');
+let blueButton = document.querySelector('#blue');
+
+redButton.addEventListener("click", () => {
+    let colorDiv = document.querySelector('#div_color');
+    colorDiv.setAttribute("class", "bg_red")
+});
+yellowButton.addEventListener("click", () => {
+    let colorDiv = document.querySelector('#div_color');
+    colorDiv.setAttribute("class", "bg_yellow")
+});
+greenButton.addEventListener("click", () => {
+    let colorDiv = document.querySelector('#div_color');
+    colorDiv.setAttribute("class", "bg_green")
+});
+blueButton.addEventListener("click", () => {
+    let colorDiv = document.querySelector('#div_color');
+    colorDiv.setAttribute("class", "bg_blue")
+});
