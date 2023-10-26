dedc66ad1966fcea85e04f0e396c3476f8afaec3
EpicCoco <forthecodeman@gmail.com>
2023-10-26 16:03:14 -0400

    Added UI file and other changes

--- Class Assignments/expenses_starter/src/components/Expenses/ExpenseItem.js
+++ Class Assignments/expenses_starter/src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,27 @@
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
-//import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
 
   const [title, setTitle] = useState(props.title);
   console.log('ExpenseItem evaluated by React');
-
+  
   const clickHandler = () => {
     setTitle('Updated!');
     console.log(title);
   };
 
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
   );
 }
 
