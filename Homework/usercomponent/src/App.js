67228df8277b5c87e32db705eb11a9e54d5c3f3e
EpicCoco <forthecodeman@gmail.com>
2023-10-31 16:49:56 -0400

    Cleaned files, started UsersList

--- Homework/usercomponent/src/App.js
+++ Homework/usercomponent/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import User from './components/User';
 import AddUser from './components/AddUser';
 import Hdr from './components/Hdr';
+import UsersList from './components/UsersList';
 
 function App() {
   return (
     <div>
       <Hdr />
       <AddUser />
-      <User />
+      <UsersList users={allUsers}/>
     </div>
   );
 }
---
+++ Homework/usercomponent/src/components/UsersList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from './Card';
+import User from './User';
+import './UsersList.css';
+import './User.css';
+
+const UsersList = (props) => {
+
+    return (
+        <Card className="users">
+            <ul>
+                {props.users.map((user) => {
+                    <User
+                        key={user.id}
+                        img={user.img}
+                        name={user.name}
+                        age={user.age}
+                        major={user.major}
+                    />
+                })}
+            </ul>
+        </Card>
+    );
+};
+
+export default UsersList;
