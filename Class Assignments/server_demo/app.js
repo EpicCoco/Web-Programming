f2aeb3b6584c54b1473e6fb6f870715f3efe8c05
EpicCoco <forthecodeman@gmail.com>
2023-11-07 17:00:38 -0500

    express routing demo

--- Class Assignments/server_demo/app.js
+++ Class Assignments/server_demo/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello world!'));
+const router = express.Router();
+router.get('/', (req, res) => {
+    res.send('root route');
+});
+router.get('/home', (req, res) => res.send('This is the home page!'));
+router.get('/login', (req, res) => {
+    res.send('this is the login page');
+});
+app.use('/', router);
 const port = process.env.PORT || 8082;
-app.listen(port, () => console.log(`Server running on port
-${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
+console.log('Web Server listening at port ' + (process.emitWarning.port || port))
