bf03f4e98a8f446aa7471e94fc72d6a4c78f840d
EpicCoco <forthecodeman@gmail.com>
2023-11-09 16:43:03 -0500

    added mongodb

--- Class Assignments/server_demo/app.js
+++ Class Assignments/server_demo/app.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const port = process.env.PORT || 8082;
+const cors = require('cors');
 const router = express.Router();
+
+app.use(cors({origin: true, credentials: true}));
+app.use(express.json({extended: false}));
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const conn_str = 'mongodb+srv://codeyborrelli:flJCpJZuFsH0Adsr@cluster0.thd6qks.mongodb.net/?retryWrites=true&w=majority';
+mongoose.set('strictQuery', false);
+mongoose.connect(conn_str, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+.then(() => {
+    app.listen(port);
+    console.log('connection successful');
+})
+.catch(err => {
+    console.log('error connecting ${err}');
+})
+
+/* old router code
 router.get('/', (req, res) => {
     res.send('root route');
 });
@@ -9,6 +32,7 @@ router.get('/login', (req, res) => {
     res.send('this is the login page');
 });
 app.use('/', router);
-const port = process.env.PORT || 8082;
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
 console.log('Web Server listening at port ' + (process.emitWarning.port || port))
+*/
