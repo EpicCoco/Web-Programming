c809fc98b0d79ddbc57d5983427fc45d3de73093
EpicCoco <forthecodeman@gmail.com>
2023-11-14 16:52:27 -0500

    updated post for default

--- Class Assignments/server_demo/routes/api/items.js
+++ Class Assignments/server_demo/routes/api/items.js
@@ -4,7 +4,13 @@ const Item = require('../../models/Item');
 
 router.get('/', (req, res) => {res.send('testing get / item route')});
 router.get('/:id', (req, res) => {res.send('testing get /:id route')});
-router.post('/', (req, res) => {res.send('testing post / route')});
+router.post('/', (req, res) => {
+    console.log(req.body);
+    Item.create(req.body)
+    .then((item) => res.json({msg: 'Item successfully added'}))    
+    .catch((err) => res.status(400).json({error: 'Unable to add item'}));
+});
+
 router.put('/:id', (req, res) => {res.send('testing put /:id route')});
 router.delete('/:id', (req, res) => {res.send('testing delete /:id route')});
 
