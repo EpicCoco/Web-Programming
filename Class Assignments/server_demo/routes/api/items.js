853e4702f5563b83208123fa9bf96fa5e9d80f2c
EpicCoco <forthecodeman@gmail.com>
2023-11-14 17:11:15 -0500

    added id server routes

--- Class Assignments/server_demo/routes/api/items.js
+++ Class Assignments/server_demo/routes/api/items.js
@@ -2,13 +2,24 @@ const express = require('express');
 const router = express.Router();
 const Item = require('../../models/Item');
 
-router.get('/', (req, res) => {res.send('testing get / item route')});
-router.get('/:id', (req, res) => {res.send('testing get /:id route')});
+router.get('/', (req, res) => {
+    Item.find()
+    .then((items) => res.json(items))
+    .catch((err) => res.status(404).json({noitemsfound: "No Items found"}));
+});
+router.get('/:id', (req, res) => {
+    Item.findById(req.params.id)
+    .then((item) => res.json(item))
+    .catch((err) => res.status(404).json({noitemfound: 'No Item found'}));    
+});
 router.post('/', (req, res) => {
     console.log(req.body);
     Item.create(req.body)
     .then((item) => res.json({msg: 'Item successfully added'}))    
-    .catch((err) => res.status(400).json({error: 'Unable to add item'}));
+    .catch((err) => {
+        console.log(err.body);
+        res.status(400).json({error: 'Unable to add item'})
+    });
 });
 
 router.put('/:id', (req, res) => {res.send('testing put /:id route')});
