1f6795de8622b8e17c80f5306422e2eb1671931e
EpicCoco <forthecodeman@gmail.com>
2023-11-16 16:53:09 -0500

    Find and delete implemented

--- Class Assignments/server_demo/routes/api/items.js
+++ Class Assignments/server_demo/routes/api/items.js
@@ -1,3 +1,4 @@
+var bodyParser = require("body-parser");
 const express = require('express');
 const router = express.Router();
 const Item = require('../../models/Item');
@@ -12,7 +13,7 @@ router.get('/:id', (req, res) => {
     .then((item) => res.json(item))
     .catch((err) => res.status(404).json({noitemfound: 'No Item found'}));    
 });
-router.post('/', (req, res) => {
+router.post('/', bodyParser.json(), (req, res) => {
     console.log(req.body);
     Item.create(req.body)
     .then((item) => res.json({msg: 'Item successfully added'}))    
@@ -20,9 +21,17 @@ router.post('/', (req, res) => {
         console.log(err.body);
         res.status(400).json({error: 'Unable to add item'})
     });
-});
+}); 
 
-router.put('/:id', (req, res) => {res.send('testing put /:id route')});
-router.delete('/:id', (req, res) => {res.send('testing delete /:id route')});
+router.put('/:id', (req, res) => {
+    Item.findByIdAndUpdate(req.params.id, req.body)
+    .then((item) => res.json({msg: 'Updated successfully'}))
+    .catch((err) => res.status(400).json({error: 'Unable to update database'}))
+});
+router.delete('/:id', (req, res) => {
+    Item.findByIdAndDelete(req.params.id)
+    .then((item) => res.json({msg: 'Item entry deleted successfully'}))
+    .catch((err) => res.status(400).json({error: 'No such item'}))
+});
 
 module.exports = router;
